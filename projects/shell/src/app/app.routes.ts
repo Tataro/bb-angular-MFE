e2f6e72b660d88e8ab0f684da52ee1b5d4d62b7e
Tataro <tat.kittitat@gmail.com>
2024-09-13 15:26:26 +0700

    chore: Add app shell, home component, and routes

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { loadRemoteModule } from '@angular-architects/native-federation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'mfe1',
+    loadComponent: () =>
+      loadRemoteModule('mfe1', './Component').then((m) => m.AppComponent),
+  },
+];
